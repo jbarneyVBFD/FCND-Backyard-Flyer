5fd92640e1a2771f4b0dc7c4adca2071d02e1af7
John Barney <jfbarney1986@yahoo.com>
2020-07-10 12:44:07 -0400

    fix small error

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -60,13 +60,12 @@ class BackyardFlyer(Drone):
             latitude = self.local_position[1]
 
             #check if lat and long are within 95% of target
-            if len( self.all_waypoints ) > 0 :
-                if longitude > 0.95 * self.target_position[0]:
-                    if latitude > 0.95 * self.target_position[1]:
+            if longitude > 0.95 * self.target_position[0]:
+                if latitude > 0.95 * self.target_position[1]:
+                    if len( self.all_waypoints ) > 0:
                         self.waypoint_transition()
-            else:
-                if longitude > 0.95 * self.target_position[0] and latitude > 0.95 * self.target_position[1]:
-                    self.landing_transition()
+                    else:
+                        self.landing_transition()
 
 
 
